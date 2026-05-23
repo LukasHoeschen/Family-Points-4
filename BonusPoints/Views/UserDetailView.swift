88fd81fbc604016c39f2850080216e5f09c4f254
Lukas Hoeschen <lukas@hoeschen.org>
2026-05-23 14:31:55 +0200

    FIX: split view detail link made problems

---
+++ BonusPoints/Views/UserDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailView.swift
+//  BonusPoints
+//
+//  Created by Lukas Marius Hoeschen on 23.05.26.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailView()
+}
